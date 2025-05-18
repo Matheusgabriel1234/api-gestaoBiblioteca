f4dbbba8dc257d0136a96fde76b604a166739b33
Matheusgabriel1234 <magaje2005@gmail.com>
2025-05-18 12:56:34 -0300

    initial commit

---
+++ src/test/java/matheus/projetos/BibliotecaGestao/BibliotecaGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus.projetos.BibliotecaGestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
