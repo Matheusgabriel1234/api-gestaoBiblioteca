d54ce3c77f509a5ccf006eb48bea039f69653bf2
Matheusgabriel1234 <magaje2005@gmail.com>
2025-05-27 14:08:25 -0300

    UsuarioService criado

--- src/main/java/matheus/projetos/BibliotecaGestao/usuario/repository/UsuarioRepository.java
+++ src/main/java/matheus/projetos/BibliotecaGestao/usuario/repository/UsuarioRepository.java
@@ -4,9 +4,11 @@ import matheus.projetos.BibliotecaGestao.usuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     boolean existsByEmail(String email);
-    Usuario findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
     Usuario findByDocument(String document);
 }
--- src/main/java/matheus/projetos/BibliotecaGestao/usuario/service/UsuarioService.java
+++ src/main/java/matheus/projetos/BibliotecaGestao/usuario/service/UsuarioService.java
@@ -5,20 +5,48 @@ import matheus.projetos.BibliotecaGestao.usuario.DTO.UsuarioDTO;
 import matheus.projetos.BibliotecaGestao.usuario.entity.Usuario;
 import matheus.projetos.BibliotecaGestao.usuario.mapper.UsuarioMapper;
 import matheus.projetos.BibliotecaGestao.usuario.repository.UsuarioRepository;
+import org.apache.el.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class UsuarioService {
 
-@Autowired
-private UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-public UsuarioDTO addUsuario(Usuario usuario){
-    Usuario savedUser = usuarioRepository.save(usuario);
-    return UsuarioMapper.convertEntityToDto(savedUser);
-}
+    public UsuarioDTO addUsuario(Usuario usuario) {
+        Usuario savedUser = usuarioRepository.save(usuario);
+        return UsuarioMapper.convertEntityToDto(savedUser);
+    }
+
+    public UsuarioDTO getUsuarioById(Long id) {
+        Usuario user = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        return UsuarioMapper.convertEntityToDto(user);
+    }
+
+    public  List<UsuarioDTO> getAllUsuarios(){
+         return usuarioRepository.findAll().stream().map(UsuarioMapper::convertEntityToDto).toList();
+    }
+
+    public UsuarioDTO getUsuarioByEmail(String email){
+        Usuario user = usuarioRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        return UsuarioMapper.convertEntityToDto(user);
+    }
+
+    public UsuarioDTO updateUsuario(Usuario newUser,Long id){
+        Usuario user = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        user.setFirstname(newUser.getFirstname());
+        user.setLastname(newUser.getLastname());
+        user.setDocument(newUser.getDocument());
+        user.setEmail(newUser.getEmail());
+        Usuario savedUser = usuarioRepository.save(user);
+        return UsuarioMapper.convertEntityToDto(savedUser);
+    }
 
 
 
